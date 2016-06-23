41b3ddbc64c65c06f10ceb4f6b545e2b469a4633
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-06-23 16:58:13 +0100

    functionality finalised

--- src/interface.js
+++ src/interface.js
@@ -2,8 +2,10 @@
 
 $(document).ready(function(){
 
-  var thermostat;
   thermostat = new Thermostat();
+  var thermostat;
+
+  apiCall();
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
@@ -16,22 +18,26 @@ $(document).ready(function(){
   $('#increase').on('click', function(){
     thermostat.increaseTemperature();
     updateTemperature();
+    g.refresh(thermostat.getCurrentTemperature());
   })
 
   $('#decrease').on('click', function(){
     thermostat.decreaseTemperature();
     updateTemperature();
+    g.refresh(thermostat.getCurrentTemperature());
   })
 
   $('#reset').on('click', function(){
     thermostat.resetTemperature();
     updateTemperature();
+    g.refresh(thermostat.getCurrentTemperature());
   })
 
   $('#PSM_on').on('click', function(){
     thermostat.enablePowerSavingMode();
     updateTemperature();
     $('#PSM_status').text('on')
+    g.refresh(thermostat.getCurrentTemperature());
   })
 
   $('#PSM_off').on('click', function(){
@@ -39,5 +45,36 @@ $(document).ready(function(){
     $('#PSM_status').text('off')
   })
 
+
+  function apiCall()  {
+    var stateCity;
+    var city;
+    stateCity = $('#city_list').val();
+    city = $("option[value='" + stateCity + "']").text();
+    $('#city').text(city);
+
+
+    $.ajax({
+      url : "http://api.wunderground.com/api/47d299c55b24b723/geolookup/conditions/q/" + stateCity + ".json",
+      dataType : "jsonp",
+      success : function(parsed_json) {
+        var location = parsed_json['location']['city'];
+        var temp_c = parsed_json['current_observation']['temp_c'];
+        $('#city_temp').text(temp_c);
+      }
+    });
+  }
+
+  $('#city_list').change(function(){
+    apiCall();
+  });
+
+  var g = new JustGage({
+    id: "gauge",
+    value: 20,
+    min: 10,
+    max: 32,
+    title: "Thermostat"
+  });
 
 });
